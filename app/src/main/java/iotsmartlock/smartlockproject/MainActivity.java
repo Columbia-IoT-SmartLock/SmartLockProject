44105a12d0c71d7724c9a9d2dc5cf5f33f249ab2
Columbia-IoT-SmartLock <vh2248@columbia.edu>
2015-09-22 13:56:32 -0400

    Updates

--- app/src/main/java/iotsmartlock/smartlockproject/MainActivity.java
+++ app/src/main/java/iotsmartlock/smartlockproject/MainActivity.java
@@ -8,10 +8,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+import java.security.Security;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final static String EXTRA_MESSAGE = "com.iotsmartlock.smartlockproject.MESSAGE";
+    public final static String LOGIN_DATA = "com.iotsmartlock.smartlockproject.MESSAGE";
+    public boolean authenticated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +37,95 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        RelativeLayout main_view = (RelativeLayout) findViewById(R.id.main_view);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Intent loggedInAct = new Intent(this, loggedInActivity.class);
+        Intent alertsAct = new Intent(this, alertsActivity.class);
+        Intent controlAct = new Intent(this, controlActivity.class);
+        Intent newUserAct = new Intent(this, newuserActivity.class);
+
+        switch(item.getItemId()) {
+
+            case R.id.menu_login:
+                // Already on login page
+                return true;
+
+            case R.id.menu_lockstatus:
+                startActivity(controlAct);
+                return true;
+
+            case R.id.menu_newuser:
+                startActivity(newUserAct);
+                return true;
+
+            case R.id.menu_alerts:
+                startActivity(alertsAct);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
         }
 
-        return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the user clicks the Send button **/
-    public void sendMessage(View view) {
-        // Do something in response to the button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    /** Called when the login button has been clicked
+     *  Retrieves username and password information (save to a variable)
+     *  Compares login information with Parse/Atmel (TBD)
+     *  If login information is correct, start logged in activity
+     *  Otherwise, display error message  **/
+    public void authenticateUser(View view) {
+
+        Intent intent = new Intent(this, loggedInActivity.class);
+
+        EditText username = (EditText) findViewById(R.id.usernameEntry);
+        EditText password = (EditText) findViewById(R.id.passwordEntry);
+
+        TextView error = (TextView) findViewById(R.id.loginStatus);
+        TextView hash = (TextView) findViewById(R.id.hashValue);
+
+        String usrMessage = username.getText().toString();
+        String pwdMessage = password.getText().toString();
+
+        intent.putExtra(LOGIN_DATA, usrMessage);
+        intent.putExtra(LOGIN_DATA, pwdMessage);
+
+        String hashed = getSHA256(pwdMessage);
+        hash.setText(hashed);
+
+        // Check if the user is authenticated
+        if (authenticated){
+            error.setText("Welcome Back!");
+            startActivity(intent);
+        } else {
+            error.setText("Login Information Incorrect");
+        }
 
     }
+
+    /* Compute SHA-256 hash value for given string */
+    public String getSHA256(String string) {
+        // Generate SHA 256 hash for user authentication
+        MessageDigest md = null;
+        byte[] testbyte = new byte[];
+
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+
+        md.reset();
+
+
+
+        return sha256;
+    }
+
+    public void sendParse(){
+
+    }
+
+
+
 }
